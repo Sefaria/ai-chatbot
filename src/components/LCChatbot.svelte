2bb1909aab134d32c9577fe210e9b9c459921f95
stevekaplan123 <steve@sefaria.org>
2026-03-15 13:29:07 +0200

    chore: dont use Infinity

--- src/components/LCChatbot.svelte
+++ src/components/LCChatbot.svelte
@@ -15,8 +15,8 @@
     'is-moderator': isModerator = false,
     'default-open': defaultOpen = false,
     mode: modeProp = 'floating',
-    'max-input-chars': maxInputChars = Infinity,
-    'max-prompts': maxPrompts = Infinity,
+    'max-input-chars': maxInputChars = 10000,
+    'max-prompts': maxPrompts = 100,
     'welcome-messages': welcomeMessagesJson = '{"welcome_english":"Hi! How can I help you today?","restart_english":"The conversation has been restarted. What would you like to talk about?","new_session_english":"Starting a new session. How can I assist you?","welcome_hebrew":"שלום! איך אפשר לעזור?","restart_hebrew":"השיחה אופסה. על מה תרצה לדבר?","new_session_hebrew":"מתחילים שיחה חדשה. איך אפשר לעזור?"}', 
     origin: originProp = '',
     'interface-lang': interfaceLang = 'english'
